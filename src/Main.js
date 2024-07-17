ee1fd075d10eb819457afa0a28b368f685389129
Mateo Castellanos <mcastellanos@northernbi.com>
2024-07-17 11:12:02 -0300

    update 07/16/2024

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,34 @@
+import greekSalad from './Greek-salad.jpg'
+import bruschetta from  './bruschetta.jpg'
+import tresLeches from './Tres-Leches.jpg'
+
 function Main(){
 return (
 <main>
-    <h2>place holder</h2>
+    <section className='specialTitle'>
+        <h2>Special</h2>
+        <button>Online Menu</button>
+    </section>
+
+    <section className='menuHightlights'> 
+        <article>
+            <img src={greekSalad} />
+            <p>Best salad you will Ever try!</p>
+        </article>
+
+        <article>
+            <img src={bruschetta} />
+            <p>Baguette with chops tomatos, best in the city</p>
+        </article>
+
+        <article>
+            <img src={tresLeches} />
+            <p>Sweet and teasty dessert, even if you don'
+                like sweet, you will like this.
+            </p>
+        </article>
+
+    </section>
 </main>)
 }
 export default Main;
